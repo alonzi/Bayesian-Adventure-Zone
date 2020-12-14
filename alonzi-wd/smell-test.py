4c300db08b383a0e59656e19f721bb160de9ced8
pete <lpa2a@virginia.edu>
2020-12-14 10:36:26 -0500

    add smell test skeleton to repo

---
+++ alonzi-wd/smell-test.py
@@ -0,0 +1,25 @@
+# pete alonzi (lpa2a@virginia.edu)
+# a smell test for the adventure zone dataset
+# 2020-12-14
+# https://github.com/alonzi/Bayesian-Adventure-Zone
+# coding practice
+
+import pandas as pd
+
+
+
+
+rolls = pd.read_csv('TravisRollsDataset.csv')
+print(rolls.head(10))
+
+
+
+
+'''
+## things i googled
+# pandas syntax read_csv
+# pandas show top ten rows of dataframe
+# install pandas
+# install anaconda
+
+'''

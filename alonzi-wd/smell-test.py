0fffeb180654e07d1fffe1eb7e5a9960bc52b5ce
pete <lpa2a@virginia.edu>
2020-12-15 22:40:49 -0500

    add a lot of notes on conda environment

--- alonzi-wd/smell-test.py
+++ alonzi-wd/smell-test.py
@@ -21,5 +21,21 @@ print(rolls.head(10))
 # pandas show top ten rows of dataframe
 # install pandas
 # install anaconda
+# when did anaconda get terrible
+# install miniconda
+# bash set environment variable
+# add directory to path
+# install pandas
+# when did windows get so terrible
+# install pandas vs code
+# vs code use conda environment
+# get list of installed packages in conda environment
+'''
 
+'''
+## conda environment setup
+# conda create -n env-smell-test
+# source /c/Users/petea_000/miniconda3/etc/profile.d/conda.sh # required to allow 'conda activate'
+# conda activate env-smell-test
+# conda install pandas
 '''
